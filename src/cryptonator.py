1c423cec4fed400c2d8cf67e7f048670aacd4c7a
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 15:17:33 +0300

    Update cryptonator.py

--- src/cryptonator.py
+++ src/cryptonator.py
@@ -1,23 +1,18 @@
-from requests import get
+from requests import Session
 
 class Cryptonator:
 	def __init__(self) -> None:
 		self.api = "https://api.cryptonator.com/api"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
 	def get_currencies(self) -> dict:
-		return get(
-			f"{self.api}/currencies",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/currencies").json()
 
 	def get_simple_ticker(self, currency_code: str) -> dict:
-		return get(
-			f"{self.api}/ticker/{currency_code}",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/ticker/{currency_code}").json()
 
 	def get_complete_ticker(self, currency_code: str) -> dict:
-		return get(
-			f"{self.api}/full/{currency_code}",
-			headers=self.headers).json()	
+		return self.session.get(f"{self.api}/full/{currency_code}").json()	
